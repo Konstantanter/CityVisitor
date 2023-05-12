b6f8ed2b13f33766a1005ffb31ca12343faeeaaa
onufriev97 <onufriev97@mail.ru>
2023-05-12 11:29:42 +0300

    Написал класс город

--- CityLibrary/City.cs
+++ CityLibrary/City.cs
@@ -4,7 +4,45 @@ using System.Text;
 
 namespace CityLibrary
 {
-    class City
+    /// <summary>
+    /// Класс описывающий город
+    /// </summary>
+    public class City
     {
+        /// <summary>
+        /// Имя города
+        /// </summary>
+        public string NameCity { get; set; }
+        /// <summary>
+        /// Краткая справка
+        /// </summary>
+        public string ShortReference { get; set; }
+        /// <summary>
+        /// Дата посещения
+        /// </summary>
+        public string DatVis { get; set; }
+        //Материалы
+        //Сувенир
+        /// <summary>
+        /// Посещен город или нет
+        /// </summary>
+        public bool IsVisited { get; private set; }
+        /// <summary>
+        /// Функция установки посещения города
+        /// </summary>
+        public void VisitedCity()
+        {
+            IsVisited = true;
+        }
+        /// <summary>
+        /// Конструктор по умолчанию
+        /// </summary>
+        public City()
+        {
+            NameCity = "";
+            ShortReference = "";
+            DatVis = "";
+            IsVisited = false;
+        }
     }
 }
